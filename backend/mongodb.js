49163da5919514bc1b77170beaadbcfd92cf8487
Nididier <76929907+dniyomukiza@users.noreply.github.com>
2024-08-15 22:54:53 -0400

    removed deprecated libraries

--- backend/mongodb.js
+++ backend/mongodb.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/userdb", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect("mongodb://localhost:27017/userdb")
   .then(() => console.log("Connected to MongoDB!"))
   .catch(err => console.error("Failed to connect to MongoDB:", err));
 
